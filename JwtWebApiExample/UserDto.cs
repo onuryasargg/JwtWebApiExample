96235e6bc02b845c7aee107860be2139e1f84780
Admin <Admin@DESKTOP-DLS1D93>
2022-07-18 22:06:23 +0300

    Add project files.

---
+++ JwtWebApiExample/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtWebApiExample
+{
+    public class UserDto // data transfer object
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
